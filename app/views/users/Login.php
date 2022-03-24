0d1da95bbbc536626901274812591e210c8ac063
basmadessouky <basma1900957@miuegypt.edu.eg>
2022-03-24 20:18:59 +0200

    Login (frontend)

--- app/views/users/Login.php
+++ app/views/users/Login.php
@@ -1,67 +1,42 @@
+<html>
+<head><link rel="stylesheet" href="<?php echo URLROOT . 'css/login.css'; ?>"></head>
+</html>
 <?php
-class Login extends view
+class Login extends View
 {
   public function output()
   {
     $title = $this->model->title;
 
     require APPROOT . '/views/inc/header.php';
-    flash('register_success');
-    $text = <<<EOT
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4"> $title</h1>
-    </div>
-  </div>
-
-  </div>
-  </div>
-  </div>
-EOT;
-    echo $text;
     $this->printForm();
     require APPROOT . '/views/inc/footer.php';
   }
 
   private function printForm()
   {
-    $action = URLROOT . 'users/login';
-    $registerUrl = URLROOT . 'users/register';
-
-    $text = <<<EOT
-    <div class="row">
-    <div class="col-md-6 mx-auto">
-    <div class="card card-body bg-light mt-5">
-    <h2>Sign Up</h2>
-    <form action="$action" method="post">
-EOT;
-
-    echo $text;
-    $this->printEmail();
-    $this->printPassword();
-
-    $text = <<<EOT
-    <div class="container">
-    <div class="checkbox mb-3 mt-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-      </div>
-      <div class="row mt-4">
-        <div class="col">
-          <input type="submit" value="Login" class="form-control btn btn-lg btn-primary btn-block">
+    ?>
+    <br>
+    <div class="login">
+    
+      <form action="" method="POST" class="formcontainer">
+        <br>
+        <h2>Login</h2>
+        <div class="form__field">
+            <input type="text" id="Email" name="email" placeholder="Email" class='input-line full-width'>
         </div>
-        <div class="col">
-          <a href="$registerUrl" class="form-control btn btn-lg btn-block">New User, Sign up</a>
+        <div class="form__field">
+            <input type="password" id="password" name="password" placeholder="Password">
         </div>
-      </div>
-      </div>
-    </form>
-    </div>
-    </div>
-    </div>
-EOT;
-    echo $text . "<a href='".URLROOT."pages/editProfile'> Profile </a>";
+        <div class="form__field">
+            <input type="submit" value="Login" id="submit" name="submit">
+        </div>
+      </form>
+      <p class="acc"> Don't have an account yet?<a style="color:blue;" href = "<?php echo URLROOT . 'users/register';?>"> Sign Up here!</a></p>
+      <br>
+  </div>
+    <br><br>
+    <?php
   }
 
   private function printEmail()
@@ -89,7 +64,7 @@ EOT;
     $text = <<<EOT
     <div class="form-group">
       <label for="$fieldName"> $label: <sup>*</sup></label>
-      <input type="$type" name="$fieldName" class="form-control form-control-lg $valid" id="$fieldName" value="$val" required="">
+      <input type="$type" name="$fieldName" class="form-control form-control-lg $valid" id="$fieldName" value="$val">
       <span class="invalid-feedback">$err</span>
     </div>
 EOT;
