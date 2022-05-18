6c579288648a1e71d15db53f6ac2ddcc8c676640
hp <mansourreem0@gmail.com>
2022-05-18 19:32:51 +0200

    homePage (backend)

--- app/models/IndexModel.php
+++ app/models/IndexModel.php
@@ -3,4 +3,10 @@ class IndexModel extends model
 {
      public $title = 'MIU SE305 Blog ' . APP_VERSION;
      public $subtitle = 'Example of MVC PHP framework for SE305';
+
+     public function getProducts(){
+
+          $this->dbh->query("SELECT p.*,i.image FROM products p, image i WHERE p.product_id=i.product_id ORDER BY RAND()LIMIT 5");
+          $result= $this->dbh->execute();
+     }
 }
