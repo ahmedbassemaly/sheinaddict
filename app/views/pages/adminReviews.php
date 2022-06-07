46705cae2156a220074b298a968c72ec2933cbbf
basmadessouky <basma1900957@miuegypt.edu.eg>
2022-06-07 21:17:30 +0200

    Admin Reviews Frontend

--- app/views/pages/adminReviews.php
+++ app/views/pages/adminReviews.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="<?php echo URLROOT . 'css/cart.css'; ?>">
+<link rel="stylesheet" href="<?php echo URLROOT . 'css/adminReviews.css'; ?>">
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
@@ -6,22 +6,48 @@
 class adminReviews extends view
 {
     public function output(){
+    
+    require APPROOT . '/views/inc/sidebar.php';
+    require APPROOT . '/views/inc/header.php';
+    $this->printForm();
+    }
+
+    public function printForm(){
     ?>
     <p>
+        <div class="container">
+        <br><h2 class="heading">Reviews</h2> <br><br>       
+        <table class="table table-hover" style="white-space: nowrap">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Firstname</th> 
+                <th>Product Name</th>
+                <th>Comment</th>
+                <th>Date</th>
+                <th>Rating</th>              
+            </tr>
+            </thead>
+            <tbody>
         <?php
         $id=0;
         $reviews = $this->model->viewReview($id);
         foreach($reviews as $review){
-            echo $review->firstName;
-            echo "<br>";
-            echo $review->comment."    ".$review->date;
-            echo "<br>";
-            echo $review->rating ."    ".$review->name;
-            echo "<br><br><br>";
+            ?>
+            <tr>
+                <td><?php echo $id+1 ?></td>
+                <td><?php echo $review->firstName; ?></td>
+                <td><?php echo  $review->name;?></td>
+                <td><?php echo $review->comment; ?></td>
+                <td><?php echo $review->date; ?></td>
+                <td><?php echo $review->rating;?></td>                
+            </tr>
+            <?php
         }
         ?>
     </p>
     <?php
     }
+
 }
 ?>
